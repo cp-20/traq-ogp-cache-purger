f239e161d93f7d5f9033b426595cffabfcd01922
cp-20 <n.naoki.1587@gmail.com>
2024-12-06 01:56:55 +0900

    🍟 🥓 🥙 説明書いた in meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,14 @@
 	import Purger from '../components/purger.svelte';
 </script>
 
+<svelte:head>
+	<title>traQ の OGP キャッシュ消す君</title>
+	<description>traQ の OGP キャッシュを消します。それだけ</description>
+</svelte:head>
+
 <div class="space-y-8">
 	<div class="space-y-2">
-		<h1 class="text-2xl font-bold">traQ OGP キャッシュ消す君</h1>
+		<h1 class="text-2xl font-bold">traQ の OGP キャッシュ消す君</h1>
 		<div class="text-gray-500">
 			<p>URLを入力して消すボタンを押してください。</p>
 			<p>消した後、新しいメッセージが投稿されてから反映されます</p>
